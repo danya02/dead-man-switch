7cc63d7b426c52cf69c002c145744f09c135ab7f
Danya Generalov <danya02@me.com>
2019-01-13 02:02:10 +0300

    Adding check for IP mismatch case.

--- client.py
+++ client.py
@@ -6,7 +6,8 @@ import base64
 ADDR='http://deadman.danya02.ru/auth/challenge'
 data = requests.get(ADDR).content
 print('<', data)
-data = base64.b64decode(data)
+ip = data.split(b':')[0]
+data = base64.b64decode(data.split(b':')[1])
 my_key = rsa.PrivateKey.load_pkcs1(open('client-priv.pem','rb').read())
 server_key = rsa.PublicKey.load_pkcs1(open('server-pub.pem', 'rb').read())
 nonce = rsa.decrypt(data,my_key)
@@ -14,4 +15,6 @@ digest = hashlib.sha3_512(nonce)
 enc = rsa.encrypt(digest.digest(), server_key)
 xmit=base64.b64encode(enc)
 print('>',xmit)
-requests.post(ADDR, data=xmit).raise_for_status()
+a = requests.post(ADDR, data=xmit)
+print(a, a.content)
+
--- server.py
+++ server.py
@@ -18,9 +18,10 @@ def hello():
 # ---------
 # 0. All network exchanges are Base64-coded.
 # 1. Client asks for challenge
-# 2. Server responds with some unique string encoded with client's public key. The challenge is associated with the client's IP
-# 3. Client decrypts message, takes a SHA3-512 hash of it, encrypts it using server's public key
-# 4. Server decrypts message, compares it with the original challenge, and acts based on whether the response is correct
+# 2. Server responds with the client's IP, a colon (":", ASCII 0x3A), and some unique string encoded with client's public key. The challenge is associated with the client's IP
+# 3. Client decrypts message, takes a SHA3-512 hash of it, encrypts it using server's public key, sends it to server with the IP it heard from the server prepended with a colon.
+# 4. If the IP transmitted is different from the one the server thinks the client has, it returns a 409 Conflict response, which it will not return in any other circumstance
+# 5. Server decrypts message, compares it with the original challenge, and acts based on whether the response is correct
 
 challenges = {}
 
@@ -43,8 +44,13 @@ def challenge():
         return base64.b64encode(rsa.encrypt(ch,peer_pub_key))
     elif request.method=='POST':
         try:
-            ch = hashlib.sha3_512(get_challenge(ip)).digest()
-            resp = rsa.decrypt(base64.b64decode(request.get_data()), my_sec_key)
+            challenge = get_challenge(ip)
+            ch = hashlib.sha3_512(challenge).digest()
+            data = request.get_data()
+            prove_ip = str(data.split(b':')[0], 'utf-8')
+            if ip!=prove_ip:
+                return 'IP mismatch: client thinks '+prove_ip+', but is actually from '+ip, 409
+            resp = rsa.decrypt(base64.b64decode(data.split(b':')[1]), my_sec_key)
             if ch == resp:
                 with open(PATH+'last-login-time.txt','w') as o:
                     o.write(str(time.time()))
@@ -52,7 +58,7 @@ def challenge():
                 return 'Challenge passed!'
         except:
             print('Error while decrypting.')
-        return 'Challenge failed!', 403
+        return 'Challenge failed!', 401
 
 if __name__ == '__main__':
     app.run('127.0.0.1',5001)
