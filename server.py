e31466b8c96878ec5e9e14086bb6c1bb0795c11d
Danya Generalov <danya@danya02.ru>
2020-04-11 19:43:24 +0300

    Starting over.

--- server.py
+++ server.py
@@ -1,83 +1,36 @@
-#!/usr/bin/python3
-from flask import Flask, request
-import time
-import rsa
-#import secrets
-import json
-import base64
-import hashlib
-import os
-import traceback
-app = Flask(__name__)
+from peewee import *
+from flask import Flask
+import datetime
 
-PATH = '/var/dead-man-switch/deadman/'
+app = Flask(__name__)
+db = SqliteDatabase('dead-man.db')
+
+class MyModel(Model):
+    class Meta:
+        database = db
+
+class CheckinKey(MyModel):
+    pub_key = BlobField()
+    short_id = CharField()
+    long_id = CharField()
+    name = CharField()
+    distrusted = BooleanField()
+    trust_status_since = DateTimeField()
+
+class Checkin(MyModel):
+    used_key = ForeignKeyField(CheckinKey)
+    date = DateTimeField(default=datetime.datetime.now)
+    ip_address = IPField()
+    comment = TextField()
+    can_be_evicted = BooleanField(default=True)
+
+class Martian(MyModel):
+    date = DateTimeField(default=datetime.datetime.now)
+    ip_address = IPField()
+    content = BlobField()
+    reason = TextField()
+    associated_key = ForeignKeyField(CheckinKey, null=True)
 
 @app.route('/')
-def hello():
-    return 'This is the challenge-response server.'
-
-# Protocol:
-# ---------
-# 0. All network exchanges are Base64-coded.
-# 1. Client asks for challenge
-# 2. Server responds with the client's IP, a colon (":", ASCII 0x3A), and some unique string encoded with client's public key. The challenge is associated with the client's IP
-# 3. Client decrypts message, takes a SHA512 hash of it, encrypts it using server's public key, sends it to server with the IP it heard from the server prepended with a colon.
-# 4. If the IP transmitted is different from the one the server thinks the client has, it returns a 409 Conflict response, which it will not return in any other circumstance
-# 5. Server decrypts message, compares it with the original challenge, and acts based on whether the response is correct
-
-
-def get_challenge(ip):
-    try:
-        challenges = json.load(open('/tmp/deadman_challenges.json'))
-    except FileNotFoundError:
-        challenges = dict()
-        with open('/tmp/deadman_challenges.json','w') as o:
-            json.dump(challenges, o)
-    if ip in challenges:
-        ch = challenges.pop(ip)
-        with open('/tmp/deadman_challenges.json','w') as o:
-            json.dump(challenges, o)
-        return base64.b64decode(bytes(ch, 'utf-8'))
-    else:
-        data = os.urandom(512)
-        challenges.update({ip:str(base64.b64encode(data), 'utf-8')})
-        with open('/tmp/deadman_challenges.json','w') as o:
-            json.dump(challenges, o)
-        return data
-
-
-@app.route('/challenge',methods = ['GET'])
-def challenge_get():
-    peer_pub_key = rsa.PublicKey.load_pkcs1(open(PATH+'client-pub.pem', 'rb').read())
-    my_sec_key = rsa.PrivateKey.load_pkcs1(open(PATH+'server-priv.pem','rb').read())
-    ip = request.remote_addr
-    ch=get_challenge(ip)
-    return bytes(ip, 'utf-8')+b':'+base64.b64encode(rsa.encrypt(ch,peer_pub_key))
-
-@app.route('/response', methods=['POST'])
-def challenge_respond():
-    peer_pub_key = rsa.PublicKey.load_pkcs1(open(PATH+'client-pub.pem', 'rb').read())
-    my_sec_key = rsa.PrivateKey.load_pkcs1(open(PATH+'server-priv.pem','rb').read())
-    ip = request.remote_addr
-    try:
-        challenge = get_challenge(ip)
-        ch = hashlib.sha512(challenge).digest()
-        data = request.get_data()
-        prove_ip = str(data.split(b':')[0], 'utf-8')
-        if ip!=prove_ip:
-            return 'IP mismatch: client thinks '+prove_ip+', but is actually from '+ip, 409
-        resp = rsa.decrypt(base64.b64decode(data.split(b':')[1]), my_sec_key)
-        if ch == resp:
-            with open(PATH+'last-login-time.txt','w') as o:
-                o.write(str(time.time()))
-            try:
-                os.unlink(PATH+'latest-relative-time.txt')
-            except:pass
-            return 'Challenge passed!'
-    except:
-        # return traceback.format_exc(), 401  # DO NOT USE IN PRODUCTION!!!
-        print('Error while decrypting.')
-    return 'Challenge failed!', 401
-
-if __name__ == '__main__':
-    app.run('127.0.0.1',5001)
+def hello_world():
+    return 'Hello, World!'
